390e4abf4bb391d249cce2b35de3fd6312ad609e
luizfaias <luizfaias@gmail.com>
2010-11-29 17:34:19 -0200

    Criando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mercurio::Application.config.secret_token = '478e4656e1bc11e878c7431ae6481420986cc42bd9b54f47e81f12a894a8ee3ba507d2d9d8c6c360eb39bb909eadd30b5910976bdf9d785050bff059fa527053'
