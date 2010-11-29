617aebc325a54d38e46861d5dd9641811bae0814
André Faria <andre.java@gmail.com>
2010-11-29 18:14:55 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mercurio::Application.config.secret_token = '867d383932e513adfafed5bf3aa91c6e2789bef62472711ada10e10a9d83304ef151ce2742bc674b4e5a12c2025fa2f8374d4fc35ecfcb7793068fc60f43384f'
