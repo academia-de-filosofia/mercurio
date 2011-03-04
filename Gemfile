133734ff44ac9a5d768873c7ee40bb8ee51b11a9
luizfaias <luizfaias@gmail.com>
2011-03-04 08:44:16 -0300

    Atualizando omniauth com o devise

--- Gemfile
+++ Gemfile
@@ -1,14 +1,13 @@
 source 'http://rubygems.org'
 
-gem 'rails', '3.0.3'
+gem 'rails', '3.0.4'
 gem 'jquery-rails'
 gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'inherited_resources'
 
 # authentication
-gem 'devise'
-#gem "oa-oauth", :require => "omniauth/oauth"
-#gem "ruby-openid"
+gem 'devise', '1.2.rc'
+gem 'oa-openid', :require => 'omniauth/openid'
 
 # gem 'ruby-debug19'
 
--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,7 @@ module Mercurio
     config.i18n.default_locale = :pt_BR
 
     # JavaScript files you want as :defaults (application.js is always included).
-    config.action_view.javascript_expansions[:defaults] = %w()
+    #config.action_view.javascript_expansions[:defaults] = %w()
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
