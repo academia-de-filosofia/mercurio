05fb8f1869c25fd1682a580f42494ee09b881bd5
luizfaias <luizfaias@gmail.com>
2011-03-11 08:29:05 -0300

    Fixing ominauth with google apps using Thin

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Mercurio::Application.initialize!
+
+OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
