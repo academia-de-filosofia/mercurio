128fae77597a8a10903b42d9ce5384dc3bd7e979
luizfaias <luizfaias@gmail.com>
2011-03-11 08:34:14 -0300

    Fixing certificate path for omniauth

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Mercurio::Application.initialize!
 
-OpenID.fetcher.ca_file = "#{Rails.root}/config/ca-bundle.crt"
+OpenID.fetcher.ca_file = "#{Rails.root}/config/certs/ca-bundle.crt"
