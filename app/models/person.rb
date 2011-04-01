d4035d2a431eb832ab0af7a72c3301b70d60c05a
André Faria <andre.java@gmail.com>
2011-04-01 08:44:01 -0300

    Melhorias no Cadastro de Pessoas

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,10 @@
 class Person < ActiveRecord::Base
+   validates_length_of :name, :minimum => 7
+   validates_presence_of :name, :gender, :birth_date, :cel_phone
+   
+  
+  def status_description
+    @status ? :active : :inactive
+  end
+  
 end
