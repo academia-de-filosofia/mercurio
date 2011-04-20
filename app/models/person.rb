78a9f856f575ac3eb2970676910337a1a6df2748
Luiz Faias Jr <luizfaias@gmail.com>
2011-04-20 09:50:32 -0300

    Alterando validação na pessoa e melhorando visual com tabela zebrada

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ActiveRecord::Base
-   validates_length_of :name, :minimum => 7
-   validates_presence_of :name, :gender, :birth_date, :cell_phone
-   
+
+  validates_length_of :name, :minimum => 7
+  validates_presence_of :name, :gender, :birth_date, :home_phone
   
   def status_description
     @status ? :active : :inactive
