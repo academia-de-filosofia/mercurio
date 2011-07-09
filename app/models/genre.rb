480d8a26059b7d29c88e939aed298ec0e12bf734
André Faria <andre.java@gmail.com>
2011-07-09 14:23:07 -0300

    Ordens Padrão

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,7 @@
 class Genre < ActiveRecord::Base
   
+  default_scope order('acronym')
+  
   def to_str
     "#{acronym} - #{name}"
   end
--- app/models/media_type.rb
+++ app/models/media_type.rb
@@ -1,4 +1,6 @@
 class MediaType < ActiveRecord::Base
+
+  default_scope order('name')
   
   def to_s
     "#{name}"
