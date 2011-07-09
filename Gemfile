8d6bd59b6992084ac5dca9884f0d276cfeb92554
André Faria <andre.java@gmail.com>
2011-07-09 11:32:08 -0300

    Melhorias no Layout do Cadastro

--- Gemfile
+++ Gemfile
@@ -4,17 +4,19 @@ gem 'rails', '3.0.4'
 gem 'inherited_resources'
 gem 'jquery-rails'
 gem 'sqlite3-ruby', :require => 'sqlite3'
+gem 'haml'
 
 # authentication
 gem 'devise', '1.2.rc'
 gem 'oa-openid', :require => 'omniauth/openid'
 
 #stylesheets
-gem "compass", ">= 0.10.6"
+gem "compass"
 
 # gem 'ruby-debug19'
 
 group :test, :development do
   gem "rspec-rails"
   gem 'webrat'
+  gem 'guard'
 end
--- app/models/media.rb
+++ app/models/media.rb
@@ -1,6 +1,7 @@
 class Media < ActiveRecord::Base
   belongs_to :mediaType
   belongs_to :genre
+  default_scope joins(:genre).order('acronym, code')
 
   validates_length_of :title, :minimum => 3
   validates_presence_of :title
@@ -15,6 +16,15 @@ class Media < ActiveRecord::Base
   def upcase_title_and_author
      self.title.upcase!
      self.author.upcase!
+  end
+  
+  def formatted_code
+    "%04d" % self.code
+  end
+    
+  
+  def full_code
+    self.genre.acronym + self.formatted_code
   end
       
 end
