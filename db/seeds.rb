f637fe36d24a7f6d34e0fbfcbe780b7f67969815
André Faria <andre.java@gmail.com>
2012-03-28 08:37:43 -0300

    Tipo de Mídia

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,7 @@
 MediaType.find_or_create_by_name('Livro', :loan_days => 0,) { |m| m.id = 1 }
 MediaType.find_or_create_by_name('Revista', :loan_days => 0) { |m| m.id = 2 }
 MediaType.find_or_create_by_name('DVD', :loan_days => 0) { |m| m.id = 3 }
+MediaType.find_or_create_by_name('Monografia', :loan_days => 0) { |m| m.id = 4 }
 Media.where('media_type_id is null').update_all(:media_type_id => 1)
 
 #Media Statuses
