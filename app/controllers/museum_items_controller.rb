449efa669cddf7bb483d170c7768d98a1debbdf6
Luiz Faias Junior <junior@bluesoft.com.br>
2013-10-06 22:46:37 -0300

    Adding search for museum items

--- app/controllers/museum_items_controller.rb
+++ app/controllers/museum_items_controller.rb
@@ -1,2 +1,6 @@
 class MuseumItemsController < InheritedResources::Base
+
+  def index
+    @museum_items = MuseumItem.search(params[:query], params[:page])
+  end
 end
--- app/models/museum_item.rb
+++ app/models/museum_item.rb
@@ -4,4 +4,12 @@ class MuseumItem < ActiveRecord::Base
 
   attr_accessible :acquisition_date, :depth, :description, :height, :material, :name, :origin, :width, :picture, :inventory_number, :status, :notes
   has_attached_file :picture, :styles => { :medium => "360x270>", :thumb => "64x48>" }, :default_url => "/images/missing.png"
+  
+  def self.search(query, page = 1)    
+    if query
+      MuseumItem.where('upper(name) LIKE upper(:query) or inventory_number LIKE upper(:query)', :query => "%#{query}%").paginate :page => page, :per_page => 50
+    else
+      MuseumItem.paginate :page => page, :per_page => 20
+    end
+  end
 end
